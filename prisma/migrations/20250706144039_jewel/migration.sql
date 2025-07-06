deb0b27137f401fe20cb263f7ab567ac5624151c
gabrielteodoroo <biel.teodorob@gmail.com>
2025-07-06 11:41:14 -0300

    feat: add prisma jewel model

---
+++ prisma/migrations/20250706144039_jewel/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "jewels" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "jewels_pkey" PRIMARY KEY ("id")
+);
