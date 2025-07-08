6467d828fc22d1c5087f8f60dd7893ee3341dcab
gabrielteodoroo <biel.teodorob@gmail.com>
2025-07-08 13:11:40 -0300

    test(service): add create-service.controller e2e test

---
+++ src/infra/http/service/controllers/create-service.controller.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from '@/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { JwtService } from '@nestjs/jwt'
+import { randomUUID } from 'crypto'
+import { hash } from 'bcrypt'
+
+describe('Create service controller', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+
+  let user: {
+    id: string
+    name: string
+    email: string
+    password: string
+  }
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe())
+    prisma = moduleFixture.get(PrismaService)
+    jwt = moduleFixture.get(JwtService)
+
+    await app.init()
+
+    user = {
+      id: randomUUID(),
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: await hash('estefane123', 10),
+    }
+
+    await prisma.user.create({
+      data: user,
+    })
+  })
+
+  test('/services (POST)', async () => {
+    const token = jwt.sign(user)
+
+    const response = await request(app.getHttpServer())
+      .post('/services')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Piercing de Orelha',
+        description: 'Piercing simples na orelha',
+        price: 50.0,
+        category: 'Piercing',
+      })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
