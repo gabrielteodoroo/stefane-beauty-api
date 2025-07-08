5637e0fe35b7eed9c8f21d8f63ab0cd5730f882a
gabrielteodoroo <biel.teodorob@gmail.com>
2025-07-08 13:15:50 -0300

    feat(prisma): add jewel prisma mapper

---
+++ src/infra/database/prisma/mappers/jewel-prisma-mapper.ts
@@ -0,0 +1,35 @@
+import { Jewel as JewelDatabase } from '@prisma/client'
+import Jewel from '@/domain/jewel/entities/jewel'
+import Identity from '@/core/entities/identity'
+
+export class JewelPrismaMapper {
+  static toDomain(entity: JewelDatabase): Jewel {
+    return Jewel.create(
+      {
+        name: entity.name,
+        description: entity.description ?? undefined,
+        price: entity.price,
+        stock: entity.stock,
+        category: entity.category,
+        material: entity.material,
+        imageUrl: entity.imageUrl,
+      },
+      new Identity(entity.id),
+    )
+  }
+
+  static toPersistence(entity: Jewel): JewelDatabase {
+    return {
+      id: entity.id.toString(),
+      name: entity.name,
+      description: entity.description ?? null,
+      price: entity.price,
+      stock: entity.stock,
+      category: entity.category,
+      material: entity.material,
+      imageUrl: entity.imageUrl,
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    }
+  }
+}
