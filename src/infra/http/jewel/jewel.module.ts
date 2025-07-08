64c4eb9a520decc7cfe1332e156203c97fec9f2d
gabrielteodoroo <biel.teodorob@gmail.com>
2025-07-08 13:16:08 -0300

    feat(jewel): add jewel http module

---
+++ src/infra/http/jewel/jewel.module.ts
@@ -0,0 +1,52 @@
+import { Module } from '@nestjs/common'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { CreateJewelUseCase } from '@/domain/jewel/use-cases/create-jewel'
+import { JewelRepository } from '@/domain/jewel/repositories/jewel-repository'
+import { CreateJewelController } from './controllers/create-jewel.controller'
+import { GetJewelUseCase } from '@/domain/jewel/use-cases/get-jewel'
+import { GetJewelController } from './controllers/get-jewel.controller'
+import { ListJewelUseCase } from '@/domain/jewel/use-cases/list-jewel'
+import { ListJewelController } from './controllers/list-jewel.controller'
+import { DeleteJewelController } from './controllers/delete-jewel.controller'
+import { DeleteJewelUseCase } from '@/domain/jewel/use-cases/delete-jewel'
+
+@Module({
+  imports: [DatabaseModule],
+  providers: [
+    {
+      provide: CreateJewelUseCase,
+      useFactory: (jewelRepository: JewelRepository) => {
+        return new CreateJewelUseCase(jewelRepository)
+      },
+      inject: [JewelRepository],
+    },
+    {
+      provide: GetJewelUseCase,
+      useFactory: (jewelRepository: JewelRepository) => {
+        return new GetJewelUseCase(jewelRepository)
+      },
+      inject: [JewelRepository],
+    },
+    {
+      provide: ListJewelUseCase,
+      useFactory: (jewelRepository: JewelRepository) => {
+        return new ListJewelUseCase(jewelRepository)
+      },
+      inject: [JewelRepository],
+    },
+    {
+      provide: DeleteJewelUseCase,
+      useFactory: (jewelRepository: JewelRepository) => {
+        return new DeleteJewelUseCase(jewelRepository)
+      },
+      inject: [JewelRepository],
+    },
+  ],
+  controllers: [
+    CreateJewelController,
+    GetJewelController,
+    ListJewelController,
+    DeleteJewelController,
+  ],
+})
+export class JewelModule {}
