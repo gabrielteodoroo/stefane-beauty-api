641841cad668647b810505c14083270e130c95ac
gabrielteodoroo <biel.teodorob@gmail.com>
2025-07-16 09:05:18 -0300

    chore: update main entry point

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  console.log('JWT_TOKEN:', process.env.JWT_TOKEN) // Debug: ver se a variável está sendo lida
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
